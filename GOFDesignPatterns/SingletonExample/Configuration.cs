9a5ba70d858fde029f57986599dd3de0872299d5
Jakob Ferdinand Wegenschimmel <j.wegenschimmel@gmail.com>
2017-10-18 10:56:31 +0200

    Singelton

---
+++ GOFDesignPatterns/SingletonExample/Configuration.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SingletonExample
+{
+    internal class Configuration : IDisposable
+    {
+        private Configuration()
+        { }
+
+        private static Configuration instance;
+        public static Configuration Instance => instance ?? (instance = new Configuration());
+
+        public void Load() { /* Lade die Konfiguration */ }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GOFDesignPatterns/SingletonExample/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SingletonExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (Configuration.Instance)
+            {
+
+            }
+        }
+    }
+}
